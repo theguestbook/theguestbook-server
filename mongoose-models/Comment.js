6aebb849ab4aa7c33983bd4545007821f5b676d7
Bradley Bain <brad_bain@live.com>
2013-10-31 18:24:30 -0500

    change id from ObjectId to Number

--- mongoose-models/Comment.js
+++ mongoose-models/Comment.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 
 var commentSchema = mongoose.Schema({
 	content: String,
-	parent: ObjectId
+	parent: Number
 });
 
 exports.model = mongoose.model('Comment', commentSchema);
